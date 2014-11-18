db10dd382436d7a4472e67cebfb1d78f617f7d2b
nbaptist16 <nbaptist16@punahou.edu>
2014-11-17 15:10:13 -1000

    initial commit
    working moving dot, boundaries implemented

---
+++ maze_game/maze-game.ino
@@ -0,0 +1,79 @@
+/*
+Moving_Dot.pde
+ 
+ Example file using the The Meggy Jr Simplified Library (MJSL)
+  from the Meggy Jr RGB library for Arduino
+   
+ Blink a damned LED.
+   
+   
+ 
+ Version 1.25 - 12/2/2008
+ Copyright (c) 2008 Windell H. Oskay.  All right reserved.
+ http://www.evilmadscientist.com/
+ 
+ This library is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ 	  
+ */
+
+ 
+ 
+ 
+ 
+
+/*
+ * Adapted from "Blink,"  The basic Arduino example.  
+ * http://www.arduino.cc/en/Tutorial/Blink
+ */
+
+#include <MeggyJrSimple.h>    // Required code, line 1 of 2.
+
+// Global variables go here...
+int x = 3;
+int y = 4;
+
+void setup()                    // run once, when the sketch starts
+{
+  MeggyJrSimpleSetup();      // Required code, line 2 of 2.
+}
+
+void loop()                     // run over and over again
+{
+  CheckButtonsPress();
+  if(Button_Right)
+   {
+     if (x < 7)
+     {
+       x = x + 1;  // Also x++;
+     }
+     else
+     {
+       x = 0;
+     }
+   }
+   
+   DrawPx(x,y,Red);  //Draws dot
+   DisplaySlate();
+   delay(150);
+   ClearSlate();
+   
+   CheckButtonsPress();
+   if (Button_Left)
+   {
+     if (x < 0)
+     {
+       x = x -1
+}
+
+
